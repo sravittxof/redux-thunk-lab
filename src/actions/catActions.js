93199fd763e00de31313383a45f73812f531dce5
sravittxof <travis.fox90@gmail.come>
2021-09-21 18:23:02 -0400

    commit tree 56df45ef70f8c2f7470389d45de01fe60277f3ff

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,15 @@
 // write and export your action creator function here
+export const fetchCates = () => {
+    return (dispatch) => {
+        dispatch({type: "LOADING_CATS"});
+        fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+        .then((response) => {
+            return response.json();
+        })
+        .then((responseJSON) => {
+            // instead of logging here, call dispatch and send the cat JSON data to your store
+            console.log(responseJSON.images);
+            dispatch({type: "ADD_CATS", cats: responseJSON.images})
+        });
+    }
+}
